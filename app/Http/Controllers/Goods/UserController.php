6bff5cc55493aed18f01f241f79818233989ab62
wumenglin <1501113246@qq.com>
2020-06-20 09:31:33 +0800

    postMan

--- app/Http/Controllers/Goods/UserController.php
+++ app/Http/Controllers/Goods/UserController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Contracts\Redis;
+use Illuminate\Support\Facades\Redis;
 
 class UserController extends Controller
 {
     public function reg(){
         $name1="zhangsan";
-        $val=Redis::get($name1);
+        $val=Redis::get($name1);echo "$val:".$val;
         var_dump($val);
-        echo "$val:".$val;
+
     }
 }
